3d6d3359fb6ff4b398707721f50af07c8ab2e946
Sophie Wong <42300517+sophiewongsp@users.noreply.github.com>
2023-11-08 20:59:06 +0800

    Change localhost

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	String url = "http://localhost:80";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
